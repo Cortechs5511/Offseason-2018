894a57dd33e18b4ba7d979a05941b499a236a42c
avgupta456 <avgupta456@gmail.com>
2018-07-17 08:44:51 -0400

    Command-Control DT Code
    Sets up structure to add more mechanisms easily.

---
+++ Abhijit/mechanisms/drivetrain.py
@@ -0,0 +1,38 @@
+import wpilib
+import math
+from wpilib.command.subsystem import Subsystem
+
+class Drivetrain(Subsystem):
+    
+    def __init__(self, DTLeftMCs, DTRightMCs, leftEncoder, rightEncoder):
+        self.DTLeftMCs = DTLeftMCs
+        self.DTRightMCs = DTRightMCs
+        self.leftEncoder = leftEncoder
+        self.rightEncoder = rightEncoder
+
+    def tank(self,left,right):
+        dbLimit = 0.1
+        k = -2
+        maxSpeed = 0.85
+        
+        if(abs(left)<dbLimit):
+            left = 0
+        if(abs(right)<dbLimit):
+            right = 0
+        
+        left = (math.e**(k*left)-1) / (math.e**k-1)
+        right = (math.e**(k*right)-1) / (math.e**k-1)
+        
+        left *= maxSpeed
+        right *= maxSpeed
+        
+        self.DTLeftMCs[0].set(left)
+        self.DTRightMCs[0].set(right)
+        
+    def clearEncoders(self):
+        self.left_encoder.reset()
+        self.right_encoder.reset()
+        
+    def stop(self):
+        self.DTLeftMCs[0].set(0)
+        self.DTRightMCs[0].set(0)
