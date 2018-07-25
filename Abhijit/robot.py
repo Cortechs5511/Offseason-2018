542e6c2136475c479623ad9d67a5609e12e45d09
avgupta456 <avgupta456@gmail.com>
2018-07-25 17:55:45 -0400

    Fixed Test Case Issue
    Was calling drivetrain.auto_init when such a command did not exist.

--- Abhijit/robot.py
+++ Abhijit/robot.py
@@ -11,33 +11,51 @@ class MyRobot(wpilib.IterativeRobot):
     def robotInit(self):
         self.leftStick = wpilib.Joystick(0)
         self.rightStick = wpilib.Joystick(1)
-        
+
         TalonLeft = Talon(10)
         VictorLeft1 = Victor(11)
         VictorLeft2 = Victor(12)
         VictorLeft1.set(Victor.ControlMode.Follower,10)
         VictorLeft2.set(Victor.ControlMode.Follower,10)
         DTLeftMCs = [TalonLeft,VictorLeft1,VictorLeft2]
-        
+
         TalonRight = Talon(20)
         VictorRight1 = Victor(21)
         VictorRight2 = Victor(22)
         VictorRight1.set(Victor.ControlMode.Follower,20)
         VictorRight2.set(Victor.ControlMode.Follower,20)
         DTRightMCs = [TalonRight,VictorRight1,VictorRight2]
-        
+
         leftEncoder = wpilib.Encoder(0,1)
         rightEncoder = wpilib.Encoder(2,3)
-        
+
         self.drivetrain = DT.Drivetrain(DTLeftMCs,DTRightMCs,leftEncoder,rightEncoder)
 
+    def robotPeriodic(self):
+        pass
+
     def autonomousInit(self):
-        self.drivetrain.auto_init()
+        pass
+
+    def autonomousPeriodic(self):
+        pass
+
+    def teleopInit(self):
+        pass
 
     def teleopPeriodic(self):
         self.drivetrain.tank(self.leftStick.getY(),self.rightStick.getY())
 
+    def testInit(self):
+        pass
+
     def testPeriodic(self):
+        pass
+
+    def disabledInit(self):
+        pass
+
+    def disabledPeriodic(self):
         pass
 
 if __name__ == '__main__':
