e2e06d7d79b463c3269a333942719f2a0adba69d
SriyaM <sriyam03@gmail.com>
2018-08-14 15:19:06 -0400

    Command Based project
    Includes basic drive and mechanisms

---
+++ Command Project/oi.py
@@ -0,0 +1,16 @@
+from wpilib.joystick import Joystick
+from wpilib.buttons.joystickbutton import JoystickButton
+
+from commands.crash import Crash
+
+
+def getJoystick():
+    '''
+    Assign commands to button actions, and publish your joysticks so you
+    can read values from them later.
+    '''
+
+    joystick = Joystick(0)
+
+
+    return joystick
