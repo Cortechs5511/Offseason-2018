a21eac162ea1e5347caeda3e328eb70d027dd3a4
SriyaM <sriyam03@gmail.com>
2018-09-13 13:08:05 -0400

    simple command groups

--- Current/Command/commands/Sequences.py
+++ Current/Command/commands/Sequences.py
@@ -15,5 +15,54 @@ def IntakePosition():
     cg.addParallel(setFixedIntake(0.5))
     return cg
 
+def Level2IntakePosition():
+    cg = CommandGroup("Level2IntakePositon")
+    cg.addParallel(setPositionLift(13))
+    cg.addParallel(setPositionWrist(110))
+    cg.addParallel(setFixedIntake(0.5))
+    return cg
+
+def Level3IntakePosition():
+    cg = CommandGroup("Level3IntakePositon")
+    cg.addParallel(setPositionLift(21))
+    cg.addParallel(setPositionWrist(110))
+    cg.addParallel(setFixedIntake(0.5))
+    return cg
+
+def SwitchPosition():
+    cg = CommandGroup("IntakePositon")
+    cg.addParallel(setPositionLift(25))
+    cg.addParallel(setPositionWrist(90))
+    cg.addParallel(setFixedIntake(0))
+    return cg
+
+def SwitchShoot():
+    cg = CommandGroup("SwitchShoot")
+    cg.addParallel(setPositionLift(25))
+    cg.addParallel(setPositionWrist(90))
+    cg.addParallel(setFixedIntake(-0.4))
+    return cg
+
+def ExchangePosition():
+    cg = CommandGroup("ExchangePosition")
+    cg.addParallel(setPositionLift(0))
+    cg.addParallel(setPositionWrist(110))
+    cg.addParallel(setFixedIntake(-0.5))
+    return cg
+
+def ExchangeShoot():
+    cg = CommandGroup("ExchangeShoot")
+    cg.addParallel(setPositionLift(0))
+    cg.addParallel(setPositionWrist(110))
+    cg.addParallel(setFixedIntake(-0.5))
+    return cg
+
+def ProtectPosition():
+    cg = CommandGroup("ProtectPosition")
+    cg.addParallel(setPositionLift(0))
+    cg.addParallel(setPositionWrist(-100))
+    cg.addParallel(setFixedIntake(0))
+
+
 def UpdateDashboard():
     SmartDashboard.putData("Intake_Position", IntakePosition())
