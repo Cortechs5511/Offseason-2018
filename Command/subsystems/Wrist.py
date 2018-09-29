16d2351839ddd505b6329ce93b229fb259f226d8
avgupta456 <avgupta456@gmail.com>
2018-09-28 20:57:24 -0400

    Wrist Fix

--- Command/subsystems/Wrist.py
+++ Command/subsystems/Wrist.py
@@ -42,9 +42,38 @@ class Wrist(Subsystem):
 
         self.wrist.setQuadraturePosition(0,timeout)
 
+        [kP,kI,kD,kF] = [0.60, 0.00, 5.00, 0.00]
+
+        self.wristController = wpilib.PIDController(kP, kI, kD, kF, self, output=self)
+        self.wristController.setInputRange(-30, 120) #input range in degrees
+        self.wristController.setOutputRange(-0.8, 0.8) #output range in percent
+        self.wristController.setAbsoluteTolerance(2.0) #tolerance in degrees
+        self.wristController.setContinuous(False)
+
+    def pidGet(self):
+        return self.getAngle()
+
+    def setPIDSourceType(self):
+        return 0
+
+    def getPIDSourceType(self):
+        return 0
+
+    def pidWrite(self, output):
+        SmartDashboard.putNumber("WristError", self.wristController.getError())
+        angle = self.getAngle()
+        if output > 0 and angle > 0:
+            output = min(output, 0.25) + math.cos(angle) * 0.25 * output
+        elif output < 0 and angle > 0:
+            output = -1
+        elif output < 0 and angle < 0:
+            output = max(output, -0.25)
+        SmartDashboard.putNumber("WristPID_Out", output)
+        self.__setSpeed__(output)
+
     def getAngle(self):
         #SmartDashboard.putNumber("WristGravity", math.radians(self.wrist.getSelectedSensorPosition(0)*self.posConv))
-        SmartDashboard.putNumber("WristAngle", self.wrist.getSelectedSensorPosition(0)*self.posConv)
+        #SmartDashboard.putNumber("WristAngle", self.wrist.getSelectedSensorPosition(0)*self.posConv)
         return math.radians(self.wrist.getSelectedSensorPosition(0)*self.posConv)
 
     def getRawPosition(self):
@@ -60,21 +89,30 @@ class Wrist(Subsystem):
     def getOutputCurrent(self):
         return self.wrist.getOutputCurrent()
 
-    def setSpeed(self, speed):
+    def setSpeed(self,speed):
+        self.wristController.disable()
+        self.__setSpeed__(speed)
+
+    def __setSpeed__(self, speed):
         """ Moves wrist up if speed is negative. """
         powerpercentage = SmartDashboard.getNumber("WristPowerPercentage", 0.7)
         power = (powerpercentage * (speed)) + (self.getGravity()) *  (math.sin(self.getAngle()) * (1-powerpercentage))
         self.wrist.set(power)
         SmartDashboard.putNumber("WristPower",power)
 
-    def setSpeedNoG(self, speed):
+    def setSpeedNoG(self,speed):
+        self.wristController.disable()
+        self.__setSpeedNoG__(speed)
+
+    def __setSpeedNoG__(self, speed):
         self.wrist.set(speed)
 
-    def setSpeed2(self, speed):
-        power = speed + self.getGravity() * math.sin(self.getAngle())
+    def setAngle(self, angle):
+        self.wristController.setSetpoint(self.setpoint)
+        self.wristController.enable()
 
     def zero(self):
-        self.wrist.setQuadraturePosition(0,0)
+        self.wrist.setQuadraturePosition(0)
 
     def initDefaultCommand(self):
         self.setDefaultCommand(setFixedWrist(0,))
