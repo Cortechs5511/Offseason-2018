fb708303e9f88f34e6948da58424dbae9d6103ac
Sushrit Pasumarthy <sushritp1@gmail.com>
2018-11-08 19:18:48 -0500

    Axis Intake
    Axis version of Intake Wheel Spin.

--- Examples/robot.py
+++ Examples/robot.py
@@ -9,6 +9,7 @@ class MyRobot(wpilib.TimedRobot):
         self.Intake1 = ctre.WPI_TalonSRX(50)
         self.Intake2 = ctre.WPI_TalonSRX(51)
         self.Intake2.follow(self.Intake1)
+        AxisPowers = wpilib.Joystick(2)
 
     def teleopInit(self):
         self.count = 0
@@ -16,7 +17,8 @@ class MyRobot(wpilib.TimedRobot):
     def teleopPeriodic(self):
         self.count += 1
         sd.putNumber("count", self.count)
-        self.Intake1.set(-0.5)
+        west = AxisPowers.getAxis(5)
+        self.Intake1.set(west)
 
 if __name__ == '__main__':
     wpilib.run(MyRobot)
