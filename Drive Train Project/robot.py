aa18c3531d1696c1ddf22e5fce912c4d9b8c9980
jacob-sasser <44883537+jacob-sasser@users.noreply.github.com>
2018-12-15 14:25:57 -0500

    Drive train project code

---
+++ Drive Train Project/robot.py
@@ -0,0 +1,72 @@
+#imports important packages for running
+import wpilib
+import wpilib.drive
+from wpilib import SmartDashboard as sd
+import ctre
+import math
+
+#class of robot
+class MyRobot(wpilib.TimedRobot):
+    #declares the motors in existence
+    def robotInit(self):
+        self.Leftdrive1 = ctre.WPI_VictorSP(0)
+        self.Leftdrive2 = ctre.WPI_VictorSP(1)
+        self.Rightdrive1 = ctre.WPI_VictorSP(2)
+        self.Rightdrive2 = ctre.WPI_VictorSP(3)
+
+        self.leftJoystick = wpilib.Joystick(0)
+        self.rightJoystick = wpilib.Joystick(1)
+        self.left_encoder = wpilib.Encoder(0,1)
+
+    def teleopInit(self):
+            self.count = 0
+
+
+    def teleopPeriodic(self):
+        self.rawdistance = left_encoder.getdistance()
+        self.count+= 1
+        sd.putNumber("Count",self.count)
+        ticks = sd.getNumber("Ticks", 255)
+        radius = 4
+        distance = (2*math.pi*radius*self.rawdistance)
+        self.encoderdistance = distance/ticks
+        sd.putNumber("Encoderdistance",self.encoderdistance)
+
+
+        left = self.leftJoystick.getRawAxis(1)*0.9
+        if abs(left) < .1:
+            drive(0,0)
+        if abs(left) > 1:
+            drive(.9,.9)
+
+        if abs(right) < .1:
+                drive(0,0)
+
+        if abs(right) > 1:
+                drive(.9,.9)
+        right = self.rightJoystick.getRawAxis(1)*0.9
+        self.drive(left, right)
+
+    def Leftdrivecontrol(self, leftPower):
+
+        self.Leftdrive1.set(leftPower)
+        self.Leftdrive2.set(leftPower)
+
+    def Rightdrivecontrol(self, rightPower):
+
+        self.Rightdrive1.set(rightPower)
+        self.Rightdrive2.set(rightPower)
+
+    def drive(self, leftPower,rightPower):
+        self.Leftdrivecontrol(leftPower)
+        self.Rightdrivecontrol(rightPower)
+
+    def autonomousInit(self):
+        #creates a time to run
+        self.count = 0
+        self.autonTimer = wpilib.Timer()
+        self.autonTimer.start()
+    #def autonomousPeriodic(self):
+    #def disabledPeriodic(self):
+if  __name__ == '__main__':
+    wpilib.run(MyRobot)
--- Individual/Jacob/robot.py
+++ Individual/Jacob/robot.py
@@ -2,14 +2,40 @@ import wpilib
 import wpilib.drive
 from wpilib import SmartDashboard as sd
 import ctre
-
+import math
+#import wpilib.Timer
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
+
+        self.leftjoystick = wpilib.Joystick(0)
+        self.rightjoystick = wpilib.Joystick(1)
+
+        self.Lift1 = ctre.WPI_TalonSRX(30)
+        self.Lift2 = ctre.WPI_TalonSRX(31)
+
         self.Intake1 = ctre.WPI_TalonSRX(50)
         self.Intake2 = ctre.WPI_TalonSRX(51)
         self.Intake2.follow(self.Intake1)
-        AxisPowers = wpilib.Joystick(2)
+
+        #drive right
+        self.driveleft1 = ctre.WPI_TalonSRX(10)
+        self.driveleft2 = ctre.WPI_VictorSPX(11)
+        self.driveleft3 = ctre.WPI_VictorSPX(12)
+        self.driveleft2.follow(self.driveleft1)
+        self.driveleft3.follow(self.driveleft1)
+        #drive right
+        self.driveright1 = ctre.WPI_TalonSRX(20)
+        self.driveright2 = ctre.WPI_VictorSPX(21)
+        self.driveright3 = ctre.WPI_VictorSPX(22)
+        self.driveright2.follow(self.driveright1)
+        self.driveright3.follow(self.driveright1)
+
+        #teleop stuff
+    def teleopDrive(self):
+        leftthrottle=-self.leftjoystick.getRawAxis(1)
+        rightthrottle=-self.rightjoystick.getRawAxis(1)
+        self.drive(leftthrottle,rightthrottle)
 
     def teleopInit(self):
         self.count = 0
@@ -17,8 +43,45 @@ class MyRobot(wpilib.TimedRobot):
     def teleopPeriodic(self):
         self.count += 1
         sd.putNumber("count", self.count)
-        west = AxisPowers.getAxis(5)
-        self.Intake1.set(west)
+
+
+        self.teleopDrive()
+        if self.leftjoystick.getButton(1):
+            self.Intake1.set(0.5)
+        elif self.rightjoystick.getButton(1):
+            self.Intake1.set(-0.5)
+        else: self.Intake1.set(0)
+
+        if self.leftjoystick.getRawButton(3):
+            self.Lift1.set(0.9)
+        elif self.rightjoystick.getRawButton(3):
+            self.Lift1.set(-0.9)
+        else: self.Lift1.set(0)
+    '''
+    def autonomousPeriodic(self):
+        if self.autonTimer.get()<4.0:
+            self.drive(0.5, 0.5)
+        else:
+            self.drive(0,0)
+    '''
+    def drive(self,left,right):
+        self.driveright1.set(-right)
+        self.driveleft1.set(-left)
+
+    def autonomousInit(self):
+        self.leftAutonEncoder = wpilib.Encoder(0,1)
+        self.autonTimer = wpilib.Timer()
+
+
+        self.autonTimer.start()
+
+    def autonomousPeriodic(self):
+        encoderTicks = self.leftAutonEncoder.get()
+        Wheelturn = encoderTicks / 255
+        while Wheelturn < (10 / math.pi ) :
+            self.drive(0.5,0.6)
+
+
 
 if __name__ == '__main__':
     wpilib.run(MyRobot)
